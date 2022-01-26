7bb8d5c4f9559cfaeafddf5557cdbf7d05778648
Bozorovvv <farhod.jonikulovich@gmail.com>
2022-01-26 20:10:34 +0500

    done 1.1

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -45,6 +45,7 @@ const Search = ({ products }) => {
         />
         <Table
           style={{
+            padding: "16px",
             borderRadius: "10px",
             boxShadow: "5px 8px 24px 5px rgba(208, 216, 243, 0.6)",
           }}
